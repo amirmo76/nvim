2729b648cc10f5613944ba2d0993df548a06b66c
Amir <amir.mohseni7697@gmail.com>
2025-08-05 05:11:59 +0330

    added new theme

--- init.lua
+++ init.lua
@@ -906,23 +906,25 @@ require('lazy').setup({
         -- change the command in the config to whatever the name of that colorscheme is.
         --
         -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-        'folke/tokyonight.nvim',
+        'rebelot/kanagawa.nvim',
         priority = 1000, -- Make sure to load this before all the other start plugins.
         config = function()
             ---@diagnostic disable-next-line: missing-fields
-            require('tokyonight').setup {
-                transparent = true,
-                styles = {
-                    comments = { italic = false }, -- Disable italics in comments
-                    sidebars = 'transparent',
-                    floats = 'transparent',
+            require('kanagawa').setup {
+                transparent = false,
+                commentStyle = { italic = false },
+                keywordStyle = { italic = false },
+                colors = {
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = 'none',
+                            },
+                        },
+                    },
                 },
             }
-
-            -- Load the colorscheme here.
-            -- Like many other themes, this one has different styles, and you could load
-            -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme 'tokyonight-night'
+            vim.cmd.colorscheme 'kanagawa-wave'
         end,
     },
 
